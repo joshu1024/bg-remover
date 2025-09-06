1b1633bbc552cf3b697bd901e52be9d6589bcb22
joshu1024 <joshuakipamet@gmail.com>
2025-09-06 13:42:52 -0700

    Delete routes directory

--- routes/imageRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import removeBgImage from "../controllers/imageController.js";
-import upload from "../middleware/multer.js";
-import auth from "../middleware/auth.js";
-const imageRouter = express.Router();
-imageRouter.post("/remove-bg", upload.single("image"), auth, removeBgImage);
-export default imageRouter;
