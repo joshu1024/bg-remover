b3e88cfc7c1546cd2e65fdc4abcaeaf5ba3e3d9b
josh1024 <joshuakipamet@gmail.com>
2025-09-06 22:58:47 -0700

    commit changes

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://bg-remover-xi-brown.vercel.app",
+    credentials: true,
+  })
+);
 
 app.use("/api/user", userRoutes);
 app.use("/api/image", imageRouter);
